a87b08c073baec56d2f4f755006f842a6d00cfe8
Taiga KIYOKAWA <40013676+taigakiyokawa@users.noreply.github.com>
2024-03-11 00:05:12 +0900

    Use tokens (#40)
    * chore: Separate scripts
    
    * chore: Install tokens to components
    
    * feat: Add styles to Button
    
    * feat: Use tokens to Button

--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import styles from './Button.module.css';
+
 type Props = {
   /** Display elements inside the button such as text label */
   children: React.ReactNode;
@@ -9,5 +11,9 @@ type Props = {
  * Button component
  */
 export const Button = ({ children, onClick }: Props) => {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button className={styles.button} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
